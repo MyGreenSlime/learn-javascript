aae1a6e871b113de0f43a337eff36625e43e4d3a
Greenslime <ruksachai.o@ku.th>
2018-09-05 23:35:03 +0700

    test-variable

---
+++ ex005/scripts.js
@@ -0,0 +1,19 @@
+var myFirstVariable;
+
+document.write(myFirstVariable);
+
+document.write("<br>");
+
+var myName = "Dondon";
+
+document.write(myName);
+
+document.write("<br>");
+
+myCar = 'BMW';
+
+var x, y, z;
+var x = 10, y = 20, z = 50;
+var serialNumber = 1234;
+
+document.write(serialNumber);
--- scripts.js
+++
@@ -1,4 +0,0 @@
-console.log("This is log message");
-console.error("This is erroe message");
-console.info("Add an info to console");
-console.warn("This is your first warning")
